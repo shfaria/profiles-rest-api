354dd7f57761e363b4f2654f76348517156311b8
shfaria <hossainsaima.f@gmail.com>
2021-09-08 05:59:42 +0600

    user profile model and manager

--- profiles_rest-project/profiles_rest_api/settings.py
+++ profiles_rest-project/profiles_rest_api/settings.py
@@ -123,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'

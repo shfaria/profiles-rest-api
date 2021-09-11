de31b10a91bfcf8363ad5f573551a5dad4ab5990
shfaria <hossainsaima.f@gmail.com>
2021-09-11 21:14:49 +0600

    added Helliapivew

--- profiles_rest-project/profiles_api/views.py
+++ profiles_rest-project/profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloAPIView(APIView):
+    """Test API View"""
+
+    def get(self, request, format= None):
+        """Returns a list of API features"""
+        an_apiview= [
+            'uses HTTP methods as function(get, post, patch, put, delete)',
+            'is similar to a traditional django view',
+            'most control over logic',
+            'mapped manually to urls'
+        ]
+        return Response({'message':"hello", 'an_apiview':an_apiview})
